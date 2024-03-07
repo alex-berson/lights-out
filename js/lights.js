3a63fde0a70544a70361f113a468bf2cb7fa2d6e
Alexander Berson <aberson@yahoo.com>
2024-03-07 05:58:28 +0100

    pattern generation

--- js/lights.js
+++ js/lights.js
@@ -1,6 +1,29 @@
-const size = 5;
+let board;
+let size = 5;
 
 const showBoard = () => document.body.style.opacity = 1;
+
+const initBoard = () => board = Array.from({length: size}, () => []);
+
+const generatePattern = () => {
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            board[i][j] = Math.round(Math.random());
+        }
+    } 
+}
+
+const fillBoard = () => {   
+
+    let cells = document.querySelectorAll('.cell');
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            cells[i * size + j].classList.toggle('on', board[i][j]);
+        }
+    }   
+}
 
 const setBoardSize = () => {
 
@@ -11,9 +34,21 @@ const setBoardSize = () => {
     document.documentElement.style.setProperty('--board-size', boardSize + 'px');
 }
 
+const disableTapZoom = () => {
+
+    const preventDefault = (e) => {e.preventDefault()};
+
+    document.body.addEventListener('touchstart', preventDefault, {passive: false});
+    document.body.addEventListener('mousedown', preventDefault, {passive: false});
+}
+
 const init = () => {
 
+    disableTapZoom();
     setBoardSize(); 
+    initBoard();
+    generatePattern();
+    fillBoard();
     showBoard();
 }
 
