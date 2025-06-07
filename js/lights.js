42e9cae909e1eff2f1784c333fc1cea477adc76c
Alexander Berson <aberson@yahoo.com>
2025-06-07 06:34:25 +0300

    dark mode

--- js/lights.js
+++ js/lights.js
@@ -1,36 +1,42 @@
 let board;
-let size = 3;
 
-const initBoard = () => board = Array.from({length: size}, () => []);
+const SIZE = 3;
 
-const showBoard = () => document.body.style.opacity = 1;
+const showBoard = () => document.body.classList.add('visible');
+
+const initBoard = () => board = Array.from({length: SIZE}, () => []);
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const puzzleSolved = () => board.every(row => row.every(sq => sq == 0));
 
-const createToggleMatrix = () => {
+const setBoardSize = () => {
 
-    const index = (i, j) => i * size + j;
+    let minSide = window.innerHeight > window.innerWidth ? window.innerWidth : window.innerHeight;
+    let cssBoardSize = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--board-size')) / 100;
+    let boardSize = Math.ceil(minSide * cssBoardSize / SIZE) * SIZE;
 
-    let matrix = Array.from({length: size ** 2}, () => Array(size ** 2).fill(0));
+    document.documentElement.style.setProperty('--board-size', `${boardSize}px`);
+}
 
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
+const generatePattern = () => {
 
-            matrix[index(i, j)][index(i, j)] = 1;
+    do {
+        for (let i = 0; i < SIZE; i++) {
+            for (let j = 0; j < SIZE; j++) {
+                board[i][j] = Math.round(Math.random());
+            }
+        } 
+    } while (gaussianEliminationMod2() == null || puzzleSolved());
 
-            if (i - 1 >= 0) matrix[index(i, j)][index(i - 1, j)] = 1;
-            if (i + 1 < size) matrix[index(i, j)][index(i + 1, j)] = 1;
-            if (j - 1 >= 0) matrix[index(i, j)][index(i, j - 1)] = 1;
-            if (j + 1 < size) matrix[index(i, j)][index(i, j + 1)] = 1;
-        }
-    }
-
-    return matrix;
+    //  board = [[1,1,1],
+    //            1,1,1],
+    //           [1,1,1]]; 
 }
 
 const gaussianEliminationMod2  = () => {
 
-    let n = size ** 2;
+    let n = SIZE ** 2;
     let target = board.flat();
     let matrix = createToggleMatrix();
 
@@ -81,155 +87,203 @@ const gaussianEliminationMod2  = () => {
     return solution;
 }
 
-const showHints = () => {
+const createToggleMatrix = () => {
 
-    let hints = document.querySelectorAll('.hint');
-    let solution = gaussianEliminationMod2();
-    let light = document.querySelector('.light');
-    let on = light.classList.contains('on');
+    const index = (i, j) => i * SIZE + j;
 
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            hints[i * size + j].classList.toggle('on', solution[i * size + j] * on);
+    let matrix = Array.from({length: SIZE ** 2}, () => Array(SIZE ** 2).fill(0));
+
+    for (let i = 0; i < SIZE; i++) {
+        for (let j = 0; j < SIZE; j++) {
+
+            matrix[index(i, j)][index(i, j)] = 1;
+
+            if (i - 1 >= 0) matrix[index(i, j)][index(i - 1, j)] = 1;
+            if (i + 1 < SIZE) matrix[index(i, j)][index(i + 1, j)] = 1;
+            if (j - 1 >= 0) matrix[index(i, j)][index(i, j - 1)] = 1;
+            if (j + 1 < SIZE) matrix[index(i, j)][index(i, j + 1)] = 1;
         }
     }
+
+    return matrix;
 }
 
-const generatePattern = () => {
-
-    do {
-        for (let i = 0; i < size; i++) {
-            for (let j = 0; j < size; j++) {
-                board[i][j] = Math.round(Math.random());
-            }
-        } 
-    } while (gaussianEliminationMod2() == null);
-
-    //  board = [[1,1,1],
-    //           [1,1,1],
-    //           [1,1,1]]; 
-}
-
-const refreshBoard = () => {   
+const redrawBoard = () => {   
 
     let cells = document.querySelectorAll('.cell');
 
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            cells[i * size + j].classList.toggle('on', board[i][j]);
+    for (let i = 0; i < SIZE; i++) {
+        for (let j = 0; j < SIZE; j++) {
+            cells[i * SIZE + j].classList.toggle('on', board[i][j]);
         }
     }   
+
+    return;
 }
 
-const newGame = (e) => {
+// const redrawBoard = async () => {
 
-    let i = document.querySelector('.i');
-    let board = document.querySelector('.board');
-    let cells = document.querySelectorAll('.cell');
-   
-    board.removeEventListener('touchstart', newGame);
-    board.removeEventListener('mousedown', newGame);
+//     let cells = [...document.querySelectorAll('.cell')];
 
-    i.classList.remove('off');
+//     await Promise.all(cells.map((cell, idx) => new Promise(resolve => {
 
-    cells.forEach(cell => cell.classList.remove('scale'));
+//             let i = Math.floor(idx / SIZE);
+//             let j = idx % SIZE;
+            
+//             if ((board[i][j] && !cell.classList.contains('on')) ||
+//                 (!board[i][j] && cell.classList.contains('on'))) {
 
-    setTimeout(() => {
-        cells.forEach(cell => cell.classList.remove('sharp'));
-    }, 20);
+//                 cell.classList.toggle('on')
 
-    generatePattern();
-    refreshBoard();
-    showHints();
-    enableTouch();
-}
+//                 cell.addEventListener('transitionend', resolve, {once: true});
 
-const showBlackSquare = () => {
+//             } else {
+//                 resolve();
+//             }
+//     })));
+// }
 
-    let cells = document.querySelectorAll('.cell');
-
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-           
-            let x = 50, y = 50;
-
-            if (i % size == 0) y = 0;
-            if (i % size == size - 1) y = 100;
-            if (j % size == 0) x = 0;
-            if (j % size == size - 1) x = 100;
-
-            cells[i * size + j].style.transformOrigin = `${x}% ${y}%`;
-            cells[i * size + j].classList.add('scale');
-
-            cells[i * size + j].addEventListener('transitionend', () => { 
-
-                cells[i * size + j].classList.add('sharp');
-
-            }, {once: true});
-        }
-    }
-}
-
-const gameOver = () => {
-
-    let i = document.querySelector('.i');
-    let light = document.querySelector('.light');
-    let board = document.querySelector('.board');
-
-    light.classList.remove('on');
-    i.classList.add('off');
-
-    disableTouch();
-    showBlackSquare();  
-
-    board.addEventListener('touchstart', newGame);
-    board.addEventListener('mousedown', newGame);
-}
 
 const toggleLights = (e) => {
     
     let cell = e.currentTarget;
-    let i = Math.floor([...cell.parentNode.children].indexOf(cell) / size);
-    let j = [...cell.parentNode.children].indexOf(cell) % size;
+    let i = Math.floor([...cell.parentNode.children].indexOf(cell) / SIZE);
+    let j = [...cell.parentNode.children].indexOf(cell) % SIZE;
 
     board[i][j] ^= 1;
 
     if (i - 1 >= 0) board[i - 1][j] ^= 1;
-    if (i + 1 < size) board[i + 1][j] ^= 1;
+    if (i + 1 < SIZE) board[i + 1][j] ^= 1;
     if (j - 1 >= 0) board[i][j - 1] ^= 1;
-    if (j + 1 < size) board[i][j + 1] ^= 1;
+    if (j + 1 < SIZE) board[i][j + 1] ^= 1;
 
-    refreshBoard();
+    redrawBoard();
     showHints();
 
-    if (puzzleSolved()) setTimeout(gameOver,0);
+    // if (puzzleSolved()) gameOver();
+
+    // if (puzzleSolved()) setTimeout(gameOver, 0);
+
+    // await sleep(0);
+
+    if (puzzleSolved()) gameOver();
 }
 
-const toggleHints = () => {
+const toggleHints = async () => {
 
-    let light = document.querySelector('.light');
+    let dot = document.querySelector('.button .dot');
+    let button = document.querySelector('.button');
 
     if (puzzleSolved()) return;
 
-    light.classList.toggle('on');
+    // button.classList.toggle('on');
+    button.classList.add('blink');
 
+    button.addEventListener('animationend', () => button.classList.remove('blink'), {once: true});
+
+    // setTimeout(showHints, 200);
+
+    await sleep(100);
+    dot.classList.toggle('on');
     showHints();
 }
 
-const enableHints = () => {
+const showHints = () => {
 
-    let light = document.querySelector('.light');
+    let solution = gaussianEliminationMod2();
+    let dot = document.querySelector('.button .dot');
+    let on = dot.classList.contains('on');
+    let dots = document.querySelectorAll('.cell .dot');
 
-    light.addEventListener('touchstart', toggleHints);
-    light.addEventListener('mousedown', toggleHints);
+    for (let i = 0; i < SIZE; i++) {
+        for (let j = 0; j < SIZE; j++) {
+            dots[i * SIZE + j].classList.toggle('on', solution[i * SIZE + j] * on);
+        }
+    }
 }
 
-const disableHints = () => {
+const gameOver = async () => {
 
-    let light = document.querySelector('.light');
+    let dot = document.querySelector('.dot');
+    let board = document.querySelector('.board');
+    let button = document.querySelector('.button');
+    let letterI = document.querySelector('.letter-i');
 
-    light.removeEventListener('touchstart', toggleHints);
-    light.removeEventListener('mousedown', toggleHints);
+    disableTouch();
+
+    // button.classList.remove('on');
+    dot.classList.remove('on');
+    letterI.classList.add('off');
+    button.classList.add('off');
+
+    await paintBlackSquare(); 
+    
+    board.addEventListener('touchstart', newGame);
+    board.addEventListener('mousedown', newGame);
+}
+
+const paintBlackSquare = async () => {
+
+    // let cells = document.querySelectorAll('.cell');
+
+    let cells = [...document.querySelectorAll('.cell')];
+
+    await Promise.all(cells.map((cell, idx) => new Promise(resolve => {
+
+            let i = Math.floor(idx / SIZE);
+            let j = idx % SIZE;
+
+    // for (let i = 0; i < SIZE; i++) {
+        // for (let j = 0; j < SIZE; j++) {
+           
+            let x = 50, y = 50;
+            // let cell = cells[i * SIZE + j];
+
+            if (i % SIZE == 0) y = 0;
+            if (i % SIZE == SIZE - 1) y = 100;
+            if (j % SIZE == 0) x = 0;
+            if (j % SIZE == SIZE - 1) x = 100;
+
+            cell.style.transformOrigin = `${x}% ${y}%`;
+            cell.classList.add('scale');
+
+            cell.addEventListener('transitionend', () => {
+
+                cell.classList.add('instant');
+
+                resolve();
+                
+            }, {once: true});
+        // }
+    // }
+    })));
+}
+
+const newGame = () => {
+
+    let board = document.querySelector('.board');
+    let cells = document.querySelectorAll('.cell');
+    let button = document.querySelector('.button');
+    let letterI = document.querySelector('.letter-i');
+   
+    board.removeEventListener('touchstart', newGame);
+    board.removeEventListener('mousedown', newGame);
+
+    letterI.classList.remove('off');
+    button.classList.remove('off');
+
+    cells.forEach(cell => cell.classList.remove('scale'));
+
+    generatePattern();
+    redrawBoard();
+
+    // await sleep(0);
+
+    requestAnimationFrame(() => {
+        cells.forEach(cell => cell.classList.remove('instant'));
+    });
+
+    enableTouch();
 }
 
 const enableTouch = () => {
@@ -252,7 +306,23 @@ const disableTouch = () => {
     });
 }
 
-const disableTapZoom = () => {
+const enableHints = () => {
+
+    let button = document.querySelector('.button');
+
+    button.addEventListener('touchstart', toggleHints);
+    button.addEventListener('mousedown', toggleHints);
+}
+
+const disableHints = () => {
+
+    let button = document.querySelector('.button');
+
+    button.removeEventListener('touchstart', toggleHints);
+    button.removeEventListener('mousedown', toggleHints);
+}
+
+const disableScreen = () => {
 
     const preventDefault = (e) => e.preventDefault();
 
@@ -260,26 +330,29 @@ const disableTapZoom = () => {
     document.body.addEventListener('mousedown', preventDefault, {passive: false});
 }
 
-const setBoardSize = () => {
-
-    let minSide = window.innerHeight > window.innerWidth ? window.innerWidth : window.innerHeight;
-    let cssBoardSize = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--board-size')) / 100;
-    let boardSize = Math.ceil(minSide * cssBoardSize / size) * size;
-
-    document.documentElement.style.setProperty('--board-size', boardSize + 'px');
-}
-
-
 const init = () => {
 
-    disableTapZoom();
+    disableScreen();
     setBoardSize(); 
     initBoard();
     generatePattern();
-    refreshBoard();
+    redrawBoard();
     showBoard();
     enableTouch();
     enableHints();
+
+    // toggleHints(); //
+
+
+    enableReset(); //
 }
 
-window.addEventListener('load', () => document.fonts.ready.then(init));
+const enableReset = () => {
+
+    let resetBtn = document.querySelector('.reset');
+
+    resetBtn.addEventListener('touchstart', newGame);
+    resetBtn.addEventListener('mousedown', newGame);
+}
+
+window.onload = () => document.fonts.ready.then(init);
